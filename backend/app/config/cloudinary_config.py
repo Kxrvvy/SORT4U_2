ce93e5992402940d84097d2078e555e7759414f5
Kirby Acoba <godwinkirby@gmail.com>
2026-03-06 22:14:48 +0800

    revise: make the pictures uploaded in cloudinary isntead of localstorage

---
+++ backend/app/config/cloudinary_config.py
@@ -0,0 +1,31 @@
+import cloudinary 
+import cloudinary.uploader
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+cloudinary.config(
+    cloud_name = os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key = os.getenv("CLOUDINARY_API_KEY"),
+    api_secret = os.getenv("CLOUDINARY_API_SECRET"),
+    secure = True
+)
+
+
+def upload_image(file):
+    try:
+        result = cloudinary.uploader.upload(file)
+        return result.get("secure_url")
+    except Exception as e:
+        print(f"Cloudinary upload error: {e}")
+        return None
+
+def delete_image(public_id):
+    try:
+        result = cloudinary.uploader.destroy(public_id)
+        return result
+    except Exception as e:
+        print(f"Cloudinary delete error: {e}")
+        return None
